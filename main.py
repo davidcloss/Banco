e6a598053110e8aed23727f6aa126ae2ab2b9502
davidcloss <davidcloss@live.com>
2023-01-20 09:36:43 -0300

    Bugs criação transação
    Agora o cabeçalho do arquivo transação só é feito uma vez

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from datetime import datetime, timedelta
 class ContaBanco:
 
     def __init__(self, nome, cpf, agencia, conta):
+        self._transacoes = self._criar_transacoes(nome, agencia, conta, self._ajusta_cpf(cpf))
         self._nome = nome
         self._cpf = self._cadastro_cpf(cpf)
         self._agencia = agencia
         self._conta = conta
         self._saldo = 0
-        self._transacoes = self._criar_transacoes(nome, agencia, conta, cpf)
+
 
     def deposito(self, valor):
         self._saldo += valor
@@ -44,8 +45,11 @@ class ContaBanco:
         return datetime.now().date()
 
     def _criar_transacoes(self, nome, agencia, conta, cpf):
-        with open(f'transacoes_{conta}_{agencia}_{nome}.txt','w',encoding='utf-8') as arquivo:
-            arquivo.write(f'{conta}|{agencia}|{nome}|{cpf}')
+        if self._verifica_cadastro_cpf(self._ajusta_cpf(cpf)) == 1:
+            pass
+        else:
+            with open(f'transacoes_{conta}_{agencia}_{nome}.txt','a',encoding='utf-8') as arquivo:
+                arquivo.write(f'{conta}|{agencia}|{nome}|{cpf}')
 
 
     def _ajusta_digitos_cpf(self, cpf):
@@ -65,6 +69,8 @@ class ContaBanco:
         return cpf
 
     def _verifica_cadastro_cpf(self, cpf):
+        with open('relacao_clientes_banco_dinheiro_barato.txt', 'a', encoding='utf-8') as arquivo:
+            pass
         with open('relacao_clientes_banco_dinheiro_barato.txt', 'r', encoding='utf-8') as arquivo:
             docs = arquivo.readlines()
             for i, doc in enumerate(docs):
@@ -92,7 +98,8 @@ class ContaBanco:
 
 
 conta_david = ContaBanco('David','028.496.678-28', '16487','0025654')
-conta_david.deposito(5000)
-conta_david.saque(946)
 conta_luan = ContaBanco('Luan', '175.930,637-95', '21553', '4816')
-conta_david.transferencia(513, conta_luan)
+conta_david.deposito(5000)
+conta_luan.deposito(7000)
+conta_luan.transferencia(3000, conta_david)
+conta_david.saque(6000)
